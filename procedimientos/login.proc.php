26b15af0fbcb4ea93090d63c8c58d66f483cbd29
Iker <imozog@outlook.com>
2025-11-10 17:27:48 +0100

    Cambios de login

---
+++ procedimientos/login.proc.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+if(isset($_REQUEST['user']) && isset($_REQUEST['password'])){
+    $_SESSION['user']=$_REQUEST['user'];
+    $_SESSION['password']=$_REQUEST['password'];
+    
+}elseif(!isset($_SESSION['mensajeError'])){
+    $_SESSION['mensajeError'];
+}
+
+
+$user=$_SESSION['user'];
+$password=md5($_SESSION['password']);
+
+
+
+$my_users= "../productos/users.txt";
+$my_file= fopen($my_users,"r") or die("Unable to open file!");
+$validacion=false; 
+
+while(!feof($my_file)){
+
+    $datos=explode(";",fgets($my_file));
+    if($user == $datos[0]){
+        
+        if($password == trim($datos[1])){
+            $validacion=true;
+            break;
+        }
+    }
+
+}
+
+fclose($my_file);
+
+if($validacion==true){
+    header("Location: ../componentes/index.php");
+    exit;
+}else{
+    $_SESSION['mensajeError']="Las credenciales son incorrectas. Inténtelo de nuevo.";
+    header('Location: ../componentes/login.php');
+    exit;
+}
+
+
+
+?>
